aa28a596b4c94b5e6acca4e86090e91bc9422389
bimarsha7 <bmrsbhandari@gmail.com>
2020-12-31 13:42:58 +0545

    navbar,route, 404 error finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import Navbar from "react-bootstrap/Navbar";
 import './App.css';
+import Routes from "./Routes";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App(){
   return (
     <div className="App container py-3">
-      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand className="font-weight-bold text-muted">
-          Enterprise Resource Planning
+      <Navbar collapseOnSelect bg="light" expand="lg" className="mb-3">
+        <LinkContainer to="/">
+        <Navbar.Brand href="/" className="font-weight-bold text-muted">
+          ERP System
         </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle/>
+      <Navbar.Collapse className="justify-content-end">
+        <Nav activeKey={window.location.pathname}>
+          <LinkContainer to="/signup">
+            <Nav.Link>Signup</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/login">
+            <Nav.Link>Login</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
       </Navbar>
+      <Routes/>
     </div>
   );
 }
